95ce6c8479566d42cc73012c347d9b1bd9281d82
Arjun059 <arjunchitara35081@gmail.com>
2026-06-21 12:37:08 +0530

    changes

--- internal/db/queries/has_permission.sql
+++ internal/db/queries/has_permission.sql
@@ -1,13 +1,30 @@
 -- name: HasPermission :one
-SELECT EXISTS (
-    SELECT 1
-    FROM users usr
-    INNER JOIN role_permissions rp
-        ON usr.role_id = rp.role_id
-    INNER JOIN permissions p
-        ON rp.permission_id = p.id
+SELECT
+    usr.id,
+    usr.name,
+    usr.role_id,
+    usr.created_at,
+    usr.updated_at,
+
+    r.resource_id,
+    r.resource,
+    r.action,
+
+    CAST(r.resource_id IS NOT NULL AS boolean) AS has_permission
+FROM users usr
+LEFT JOIN LATERAL (
+    SELECT
+        p.id AS resource_id,
+        p.resource,
+        p.action
+    FROM role_resources rr
+    JOIN resources p
+        ON rr.resource_id = p.id
     WHERE
-        usr.id = @user_id
-        AND p.resource = @resource
-        AND p.action = @action
-) AS has_permission;
+        rr.role_id = usr.role_id
+        AND p.resource = sqlc.arg(resource)
+        AND p.action = sqlc.arg(action)
+    LIMIT 1
+) r ON TRUE
+WHERE
+    usr.id = sqlc.arg(user_id);
